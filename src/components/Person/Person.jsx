5c8415d2b43b61ce186d5feee9924e3ec2a127f3
YanamiYokico <v.a.nikolojchyk@gmail.com>
2025-04-21 17:30:19 +0300

    changed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -6,7 +6,7 @@ export const Person = ({ person }) => (
     {person.age && <p className="Person__age">I am {person.age}</p>}
     {person.isMarried ? (
       <p className="Person__partner">
-        {person.partnerName} is my {person.sex === 'm' ? 'wife' : 'husband'}
+        {person.partnerName} is my {person.sex === 'f' ? 'husband' : 'wife'}
       </p>
     ) : (
       <p className="Person__partner">I am not married</p>
